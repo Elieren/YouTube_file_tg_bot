ea056a42c28ef156076cbe9fc8377cb01233a665
Elieren <kir102906@gmail.com>
2023-06-16 15:12:09 +0700

    Update

--- tg_tube.py
+++ tg_tube.py
@@ -11,6 +11,9 @@ from dotenv.main import load_dotenv
 import datetime
 from pydub import AudioSegment
 from pydub.utils import mediainfo
+import asyncio
+import aiohttp
+from telebot.async_telebot import AsyncTeleBot
 
 load_dotenv()
 token = os.environ['TOKEN']
@@ -18,89 +21,87 @@ id_telegram = os.environ['ID_TELEGRAM']
 
 id_telegram = id_telegram.split(', ')
 
-def log(text):
+async def log(text):
     with open("log", "a") as f:
         f.write(str(datetime.datetime.now())+ "\t\t" + text + " " +"\n")
 
-def video(message, url):
+async def video(message, url):
     """Скачивание видео и отправка пользователю"""
-    bot.send_message(message.chat.id, 'Пожалуйста подождите...')
-    while True:
-        try:
-            # Загружаем видео с YouTube
-            yt = YouTube(url)
-            video_buffer = io.BytesIO()
-            stream = yt.streams.get_highest_resolution()
-            stream.stream_to_buffer(video_buffer)
-            video_data = video_buffer.getbuffer()
-            video_buffer.flush()
+    await bot.send_message(message.chat.id, 'Пожалуйста подождите...')
+    
+    # Загружаем видео с YouTube
+    yt = YouTube(url)
+    video_buffer = io.BytesIO()
+    stream = yt.streams.get_highest_resolution()
+    try:
+        stream.stream_to_buffer(video_buffer)
+        video_data = video_buffer.getbuffer()
 
-            # Отправляем файл пользователю
-            while True:
-                try:
-                    bot.send_video(message.chat.id, video_data, supports_streaming=True)
-                    break
-                except:
-                    pass
-            break
+        # Отправляем файл пользователю
+        while True:
+            try:
+                await bot.send_video(message.chat.id, video_data, supports_streaming=True)
+                break
+            except:
+                pass
+    except:
+        await bot.send_message(message.chat.id, 'Error: Данное видео нельзя скачать')
 
-        except:
-            pass
-    log(f"Video sent --> {message.chat.id} {url}")
+    await log(f"Video sent --> {message.chat.id} {url}")
         
-def audio(message, url):
+async def audio(message, url):
     """Скачивание аудио и отправка пользователю"""
-    bot.send_message(message.chat.id, 'Пожалуйста подождите...')
-    while True:
-        try:
-            # Загружаем аудио с YouTube
-            yt = YouTube(url)
-            video_id = yt.video_id
-            title = yt.title
-            url = f'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
-            r = requests.get(url)
-            data = r.content
+    await bot.send_message(message.chat.id, 'Пожалуйста подождите...')
 
-            audio_buffer = io.BytesIO()
-            stream = yt.streams.get_audio_only()
-            stream.stream_to_buffer(audio_buffer)
-            audio_data = audio_buffer.getvalue()
+    # Загружаем аудио с YouTube
+    yt = YouTube(url)
+    video_id = yt.video_id
+    title = yt.title
+    url_im = f'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
+    r = requests.get(url_im)
+    data = r.content
 
-            sound = AudioSegment.from_file(io.BytesIO(audio_data), format="mp4")
-            audio_convert = io.BytesIO()
-            sound.export(audio_convert, format="mp3")
-            audio_convert = audio_convert.getvalue()
-            # Отправляем файл пользователю
-            while True:
-                try:
-                    bot.send_audio(message.chat.id, audio=audio_convert, title=title, thumb=data)
-                    break
-                except:
-                    pass
-            break
-        
-        except:
-            pass
-    log(f"Audio sent --> {message.chat.id} {url}")
+    audio_buffer = io.BytesIO()
+    stream = yt.streams.get_audio_only()
+    try:
+        stream.stream_to_buffer(audio_buffer)
+        audio_data = audio_buffer.getvalue()
 
-bot = telebot.TeleBot(token)
+        sound = AudioSegment.from_file(io.BytesIO(audio_data), format="mp4")
+        audio_convert = io.BytesIO()
+        sound.export(audio_convert, format="mp3")
+        audio_convert = audio_convert.getvalue()
+        audio_buffer.close()
+        # Отправляем файл пользователю
+        while True:
+            try:
+                await bot.send_audio(message.chat.id, audio=audio_convert, title=title, thumb=data)
+                break
+            except:
+                pass
+    except:
+        await bot.send_message(message.chat.id, 'Error: Данное аудио нельзя скачать')
 
-def notuser(message, status):
+    await log(f"Audio sent --> {message.chat.id} {url}")
+
+bot = AsyncTeleBot(token)
+
+async def notuser(message, status):
     """Отправка сообщения о запрете"""
-    bot.send_message(message.chat.id, 'Здравствуйте.\nИспользование бота для вашего профиля запрещено.')
-    log(f"{status} --> {message.chat.id} not authorized ")
+    await bot.send_message(message.chat.id, 'Здравствуйте.\nИспользование бота для вашего профиля запрещено.')
+    await log(f"{status} --> {message.chat.id} not authorized ")
 
 @bot.message_handler(commands=["start"])
-def start(message, res=False):
+async def start(message, res=False):
     if str(message.chat.id) in id_telegram:
-        bot.send_message(message.chat.id, 'Здравствуйте.\nОтправьте мне ссылку и я пришлю вам видео или аудио файл.')
-        log(f"Start --> {message.chat.id} authorized ")
+        await bot.send_message(message.chat.id, 'Здравствуйте.\nОтправьте мне ссылку и я пришлю вам видео или аудио файл.')
+        await log(f"Start --> {message.chat.id} authorized ")
     else:
-        notuser(message, "Start")
+        await notuser(message, "Start")
     print(f'New message received from user {message.chat.id}')
 
 @bot.message_handler(content_types=["text"])
-def handle_message(message):
+async def handle_message(message):
     """Обрабатываем сообщения от пользователей"""
     try:
         if str(message.chat.id) in id_telegram:
@@ -111,26 +112,27 @@ def handle_message(message):
             button1 = telebot.types.InlineKeyboardButton("Видео", callback_data=f'Видео {url}')
             button2 = telebot.types.InlineKeyboardButton("Аудио", callback_data=f'Аудио {url}')
             markup.add(button1, button2)  # Добавляем кнопки в объект markup
-            bot.send_message(message.chat.id, "Что скачать:", reply_markup=markup)
+            await bot.send_message(message.chat.id, "Что скачать:", reply_markup=markup)
         else:
             print(f'New url received from user {message.chat.id} (not authorized)')
-            notuser(message, "Text")
+            await notuser(message, "Text")
     
     except Exception as e:
         """Отправляем сообщение об ошибке пользователю"""
-        bot.send_message(message.chat.id, "Ошибка: " + str(e))
-        log(f"Error --> {str(e)}")
+        await bot.send_message(message.chat.id, "Ошибка: " + str(e))
+        await log(f"Error --> {str(e)}")
     
 
 @bot.callback_query_handler(func = lambda call: True)
-def answer(call):
+async def answer(call):
     """Обработка нажатия кнопки и запуск соответствующей функции"""
     result = call.data.split()
     if result[0] == 'Видео':
-        video(call.message, result[1])
+        await video(call.message, result[1])
     elif result[0] == 'Аудио':
-        audio(call.message, result[1])
+        await audio(call.message, result[1])
+
 
 # Запускаем телеграм-бота
 print('SERVER START')
-bot.polling(none_stop=True)
+asyncio.run(bot.polling(none_stop=True))
